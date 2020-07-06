f3a08c58f14438efd02e66812f9cf50aebf68294
Christian Flach <cmfcmf.flach@gmail.com>
2020-07-06 13:09:42 +0200

    Add PFS172

--- src/chips.js
+++ src/chips.js
@@ -1,5 +1,120 @@
 const chips = [
   {
+    name: "PFS172",
+    variants: [
+      {
+        name: "6 Pin",
+        pins: [
+          "PA4", "GND", "PA6",
+          "PA5", "VDD", "PA3",
+        ]
+      },
+      {
+        name: "8 Pin",
+        pins: [
+          "VDD", "PA6", "PA5", "PB7",
+          "PB1", "PA3", "PA4", "GND",
+        ]
+      },
+      {
+        name: "10 Pin",
+        pins: [
+          "VDD", "PA6", "PA5", "PB7", "PB4",
+          "PB1", "PA3", "PA4", "PA0", "GND",
+        ]
+      },
+      {
+        name: "14 Pin",
+        pins: [
+          "VDD", "PA7", "PA6", "PA5", "PB7", "PB4", "PB5",
+          "PB0", "PB1", "PB3", "PA3", "PA4", "PA0", "GND",
+        ]
+      },
+      {
+        name: "16 Pin",
+        pins: [
+          "VDD", "PA7", "PA6", "PA5", "PB7", "PB4", "PB5", "PB6",
+          "PB2", "PB0", "PB1", "PB3", "PA3", "PA4", "PA0", "GND",
+        ]
+      },
+    ],
+    data: {
+      "Pull-up / Pull-down Availability": {
+        color: "#FFC869",
+        pins: {
+          "pull-up":   ["PA7", "PA6", "PA5", "PA4", "PA3", "PA0", "PB7", "PB6", "PB5", "PB4", "PB3", "PB2", "PB1", "PB0"],
+          "pull-down": ["PA7", "PA6", "PA5", "PA4", "PA3", "PA0", "PB7", "PB6", "PB5", "PB4", "PB3", "PB2", "PB1", "PB0"],
+        }
+      },
+      "Maximum current": {
+        color: "#FFC869",
+        pins: {
+          "sink: 22mA":       ["PA7", "PA6", "PA5", "PA4", "PA3", "PA0",        "PB6", "PB5",        "PB3", "PB2", "PB1", "PB0"],
+          "sink: 21mA/35mA":  [                                          "PB7",               "PB4"                            ],
+
+          "drive: 11mA":      ["PA7", "PA6",        "PA4", "PA3", "PA0",        "PB6", "PB5",        "PB3", "PB2", "PB1", "PB0"],
+          "drive: 12mA":      [              "PA5",                                                                            ],
+          "drive: 11mA/23mA": [                                          "PB7",               "PB4"                            ],
+        }
+      },
+      "Timer PWM Output Pin": {
+        color: "#67CEEC",
+        pins: {
+          "T2 PWM": ["PA3", "PB4", "PB2"],
+          "T3 PWM": ["PB7", "PB6", "PB5"],
+        }
+      },
+      "ADC Input Channel": {
+        color: "#95B600",
+        pins: {
+          ADC0:  "PB0",
+          ADC1:  "PB1",
+          ADC2:  "PB2",
+          ADC3:  "PB3",
+          ADC4:  "PB4",
+          ADC5:  "PB5",
+          ADC6:  "PB6",
+          ADC7:  "PB7",
+          ADC8:  "PA3",
+          ADC9:  "PA4",
+          ADC10: "PA0",
+        }
+      },
+      "Comparator Inputs and Output": {
+        color: "#BFD366",
+        pins: {
+          "COMP +": "PA4",
+          "COMP −": ["PA4", "PA3", "PB7", "PB6"],
+          "COMP = ": "PA0"
+        }
+      },
+      "External Interrupt": {
+        color: "#FF9D07",
+        pins: {
+          INT1: ["PA4", "PB0"],
+          INT0: ["PA0", "PB5"]
+        }
+      },
+      "External Crystal Pins and Timer Clock Sources": {
+        color: "#F4D620",
+        pins: {
+          "T1 CLK": ["PA0",        "PA4"],
+          "T2 CLK": ["PA0", "PB0", "PA4"],
+          "T3 CLK": ["PA0", "PB0", "PA4"],
+          XTAL1: "PA7",
+          XTAL2: "PA6"
+        }
+      },
+      "Programming Pins": {
+        pins: {
+          ICPCK: "PA3",
+          ICPDA: "PA6",
+          RESET: "PA5"
+        }
+      }
+    }
+  },
+  {
     name: "PFS173",
     variants: [
       {

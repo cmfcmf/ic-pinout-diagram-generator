7017f5b86933d3da9a531732664f1e7cf84e7011
serisman <github@serisman.com>
2020-07-07 17:10:55 -0500

    Add PMS171B

--- src/chips.js
+++ src/chips.js
@@ -545,6 +545,126 @@ const chips = [
     ]
   },
   {
+    name: "PMS171B",
+    variants: [
+      {
+        name: "6 Pin",
+        pins: [
+          "PA4", "GND", "PA6",
+          "PA5", "VDD", "PA3"
+        ]
+      },
+      {
+        name: "8 Pin",
+        pins: [
+          "VDD", "PA7", "PA6", "PA5",
+          "PA3", "PA4", "PA0", "GND",
+        ]
+      },
+      {
+        name: "10 Pin",
+        pins: [
+          "PB7", "VDD", "PA7", "PA6", "PA5",
+          "PA3", "PA4", "PA0", "GND", "PB0",
+        ]
+      },
+      {
+        name: "14 Pin",
+        pins: [
+          "PB5", "PB6", "PB7", "VDD", "PA7", "PA6", "PA5",
+          "PA3", "PA4", "PA0", "GND", "PB0", "PB1", "PB2",
+        ]
+      },
+      {
+        name: "16 Pin",
+        pins: [
+          "PB4", "PB5", "PB6", "PB7", "VDD", "PA7", "PA6", "PA5",
+          "PA3", "PA4", "PA0", "GND", "PB0", "PB1", "PB2", "PB3"
+        ]
+      },
+    ],
+    data: [
+      {
+        ...PULL_UP_DOWN,
+        pins: {
+          "pull-up":   ["PA7", "PA6", "PA5", "PA4", "PA3", "PA0", "PB7", "PB6", "PB5", "PB4", "PB3", "PB2", "PB1", "PB0"],
+          "pull-down": [                                                 "PB6",               "PB3"                     ],
+        }
+      },
+      {
+        ...MAXIMUM_CURRENT,
+        pins: {
+          "sink: 16mA":      ["PA7", "PA6", "PA5", "PA4", "PA3", "PA0", "PB7", "PB6",               "PB2", "PB2", "PB1"       ],
+          "sink: 16mA/38mA": [                                                        "PB5", "PB4"                            ],
+          "sink: 135mA":     [                                                                                           "PB0"],
+
+          "drive: 0mA":      [              "PA5",                                                                       "PB0"],
+          "drive: 6mA":      ["PA7", "PA6",        "PA4", "PA3", "PA0",        "PB6",               "PB3", "PB2", "PB1"       ],
+          "drive: 6mA/20mA": [                                                        "PB5", "PB4"                            ],
+          "drive: 135mA":    [                                          "PB7",                                                ],
+        }
+      },
+      {
+        ...PWM_TIMER,
+        pins: {
+          "T2 PWM": ["PA3", "PB4", "PB2", "PB0"],
+          "T3 PWM": ["PB7", "PB6", "PB5"],
+        }
+      },
+      {
+        ...ADC,
+        pins: {
+          //ADC0: "PB0",
+          ADC1: "PB1",
+          ADC2: "PB2",
+          ADC3: "PB3",
+          ADC4: "PB4",
+          ADC5: "PB5",
+          ADC6: "PB6",
+          ADC7: "PB7",
+          ADC8: "PA3",
+          ADC9: "PA4",
+          ADC10: "PA0",
+          AREF: "PB1"
+        }
+      },
+      {
+        ...COMPARATOR,
+        pins: {
+          "COMP +": "PA4",
+          "COMP −": ["PA4", "PA3", "PB7", "PB6"],
+          "COMP = ": "PA0"
+        }
+      },
+      {
+        ...EXTERNAL_INTERRUPT,
+        pins: {
+          INT1: ["PB0", "PA4"],
+          INT0: ["PA0", "PB5"],
+        }
+      },
+      {
+        ...CRYSTAL_AND_TIMER_CLOCK_SOURCES,
+        pins: {
+          "T16 CLK": ["PA0",        "PA4"],
+          "T2 CLK":  ["PA0", "PB0", "PA4"],
+          "T3 CLK":  ["PA0", "PB0", "PA4"],
+          XTAL1: "PA7",
+          XTAL2: "PA6"
+        }
+      },
+      {
+        ...PROGRAMMING_PINS,
+        pins: {
+          ICPCK: "PA3",
+          ICPDA: "PA6",
+          ICVPP: "PA5",
+          ICVDD: "VDD",
+        }
+      }
+    ]
+  },
+  {
     name: "PMS150C",
     variants: [
       {

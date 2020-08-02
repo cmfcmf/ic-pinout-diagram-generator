c348509b62ed85a12b374a0028f520d36bf3b22a
Christian Flach <cmfcmf.flach@gmail.com>
2020-08-02 22:27:00 +0200

    Add 74HC595

--- src/chips.js
+++ src/chips.js
@@ -59,6 +59,74 @@ const CRYSTAL_AND_TIMER_CLOCK_SOURCES = {
 
 const chips = [
   {
+    name: "74HC595",
+    variants: [
+      {
+        name: "",
+        pins: [
+          "Q_B",
+          "Q_C",
+          "Q_D",
+          "Q_E",
+          "Q_F",
+          "Q_G",
+          "Q_H",
+          "GND",
+          "SER_OUT",
+          "SRCLR",
+          "SRCLK",
+          "RCLK",
+          "OE",
+          "SER_IN",
+          "Q_A",
+          "VCC"
+        ]
+      }
+    ],
+    data: [
+      {
+        name: "Data",
+        color: "#26B9E4",
+        pins: {
+          "Data Out": [
+            "Q_A",
+            "Q_B",
+            "Q_C",
+            "Q_D",
+            "Q_E",
+            "Q_F",
+            "Q_G",
+            "Q_H",
+          ],
+        }
+      },
+      {
+        name: "Serial In/Out",
+        color: "#BFD366",
+        pins: {
+          "Serial In": ["SER_IN"],
+          "Serial Out": ["SER_OUT"]
+        }
+      },
+      {
+        name: "Control Pins",
+        color: "#FF9D07",
+        pins: {
+          "Shift Register Clock": ["SRCLK"],
+          "Latch Clock": ["RCLK"],
+        }
+      },
+      {
+        name: "Advanced Control Pins",
+        color: "#F4D620",
+        pins: {
+          "Output Enable": ["OE"],
+          "Shift Register Clear": ["SRCLR"],
+        }
+      }
+    ]
+  },
+  {
     name: "PFS154",
     variants: [
       {

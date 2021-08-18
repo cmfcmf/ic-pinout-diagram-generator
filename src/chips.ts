c678641368988030ad103ab7ae31385bb5c50458
Cluso99 <Cluso99@users.noreply.github.com>
2021-08-18 17:48:55 +1000

    Add Parallax P1 P8X32A
    Add Parallax P1 chips, update Chip.ts

--- src/chips.ts
+++ src/chips.ts
@@ -2,5 +2,6 @@ import { chips as other } from "./chips/other";
 import { chips as padauk } from "./chips/padauk";
 import { chips as wch } from "./chips/wch";
 import { chips as propeller } from "./chips/propeller";
+import { chips as propeller1 } from "./chips/propeller1";
 
-export default [...other, ...padauk, ...wch, ...propeller];
+export default [...other, ...padauk, ...wch, ...propeller, ...propeller1];
